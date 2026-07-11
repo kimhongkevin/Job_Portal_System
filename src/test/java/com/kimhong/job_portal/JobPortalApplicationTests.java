6fe8a029e75b8827b2f8cc83702f032d93266f1c
PHOUNG BOPHAKIMHONG <140382447+kimhongkevin@users.noreply.github.com>
2026-07-11 17:50:09 +0700

    Editing JobPortalApplicationTests

--- src/test/java/com/kimhong/job_portal/JobPortalApplicationTests.java
+++ src/test/java/com/kimhong/job_portal/JobPortalApplicationTests.java
@@ -3,7 +3,14 @@ package com.kimhong.job_portal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb", // Uses an in-memory test database
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.mail.host=localhost",
+		"spring.mail.username=mocked-email@test.com",
+		"spring.mail.password=mockedpassword",
+		"jwt.secret=9a4f434520bed2b0d7b224164b1d56cc3f69ed6260a1e0b3ef1d48c823528b7e" // Dummy 256-bit key
+})
 class JobPortalApplicationTests {
 
 	@Test
