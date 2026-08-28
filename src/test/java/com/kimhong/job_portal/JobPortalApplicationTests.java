02eb2411dbf840f961c213596bfe4703a07afeaa
PHOUNG BOPHAKIMHONG <140382447+kimhongkevin@users.noreply.github.com>
2026-08-29 01:22:10 +0700

    config JobPortalApplicationTests

--- src/test/java/com/kimhong/job_portal/JobPortalApplicationTests.java
+++ src/test/java/com/kimhong/job_portal/JobPortalApplicationTests.java
@@ -2,11 +2,16 @@ package com.kimhong.job_portal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class JobPortalApplicationTests {
+
+	@MockitoBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
